30a7181584629d5bd18f4fcf576ffb9237a300f1
Major-Eth <major-tom.eth@pm.me>
2021-11-25 16:44:26 +0100

    ♻️ Updating tests for borrow

--- tests/src/ib_borrow.test.js
+++ tests/src/ib_borrow.test.js
@@ -1,31 +1,40 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import { waitForAppScreen, zemu } from './test.fixture';
+import { waitForAppScreen, zemu, genericTx } from './test.fixture';
+import { ethers } from "ethers";
 
-const BASE_SCREENS_S = (1 + 1 + 1 + 1 + 1 + 1) //YEARN + AMOUNT + VAULT + NETWORK + GAS_FEES + APPROVE
-const BASE_SCREENS_X = (1 + 1 + 1 + 1 + 1 + 1) //YEARN + AMOUNT + VAULT + NETWORK + GAS_FEES + APPROVE
+const contractAddr = "0x950027632FbD6aDAdFe82644BfB64647642B6C09";
+const AMOUNT_TO_BORROW = '4358900000000';
+const BASE_SCREENS_S = (1 + 1 + 1 + 1 + 1) //YEARN + AMOUNT + GAS_FEES + VAULT + APPROVE
+const BASE_SCREENS_X = (1 + 1 + 1 + 1 + 1) //YEARN + AMOUNT + GAS_FEES + VAULT + APPROVE
 
+// Nanos S test
 test('[Nano S] IB Borrow', zemu("nanos", async (sim, eth) => {
-  // Original TX: https://etherscan.io/tx/0xc7033601b74355ea93d4fc6ca0b01507f8bd54afefdedd8e534fd1b1657f236b
-  eth.signTransaction(
-    "44'/60'/0'/0/0",
-    "f88b821d938511ed8ec200830c076894950027632fbd6adadfe82644bfb64647642b6c0980a4c5ebeaec000000000000000000000000000000000000000000000000000003f6e2afa50026a03a4830cc8db3c3f95ac6b1859cb8671a9665936fcae065c41c12df930c630175a03b9048cfc1efdc309ce2a44fd463e340ce6e498e04f02cd2aa0482fa3d32fa42"
-  );
-  
+  const contract = new ethers.Contract(contractAddr, ['function borrow(uint256 borrowAmount)']);
+  const {data} = await contract.populateTransaction.borrow(AMOUNT_TO_BORROW);
+  let unsignedTx = genericTx;
+  unsignedTx.to = contractAddr;
+  unsignedTx.data = data;
+
+  const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
+  const tx = eth.signTransaction("44'/60'/0'/0", serializedTx);
 
   await waitForAppScreen(sim);
   await sim.navigateAndCompareSnapshots('.', 'nanos_ib_borrow', [BASE_SCREENS_S, 0]);
+  await tx;
 }));
 
+test('[Nano X] IB Borrow', zemu("nanox", async (sim, eth) => {
+  const contract = new ethers.Contract(contractAddr, ['function borrow(uint256 borrowAmount)']);
+  const {data} = await contract.populateTransaction.borrow(AMOUNT_TO_BORROW);
+  let unsignedTx = genericTx;
+  unsignedTx.to = contractAddr;
+  unsignedTx.data = data;
 
-
-test('[Nano S] IB Borrow', zemu("nanox", async (sim, eth) => {
-  // Original TX: https://etherscan.io/tx/0xc7033601b74355ea93d4fc6ca0b01507f8bd54afefdedd8e534fd1b1657f236b
-  eth.signTransaction(
-    "44'/60'/0'/0/0",
-    "f88b821d938511ed8ec200830c076894950027632fbd6adadfe82644bfb64647642b6c0980a4c5ebeaec000000000000000000000000000000000000000000000000000003f6e2afa50026a03a4830cc8db3c3f95ac6b1859cb8671a9665936fcae065c41c12df930c630175a03b9048cfc1efdc309ce2a44fd463e340ce6e498e04f02cd2aa0482fa3d32fa42"
-  );
+  const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
+  const tx = eth.signTransaction("44'/60'/0'/0", serializedTx);
 
   await waitForAppScreen(sim);
   await sim.navigateAndCompareSnapshots('.', 'nanox_ib_borrow', [BASE_SCREENS_X, 0]);
+  await tx;
 }));
--- tests/src/ib_repay_borrow.test.js
+++ tests/src/ib_repay_borrow.test.js
@@ -1,30 +1,40 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import { waitForAppScreen, zemu } from './test.fixture';
+import { waitForAppScreen, zemu, genericTx } from './test.fixture';
+import { ethers } from "ethers";
 
-const BASE_SCREENS_S = (1 + 1 + 1 + 1 + 1 + 1) //YEARN + AMOUNT + VAULT + NETWORK + GAS_FEES + APPROVE
-const BASE_SCREENS_X = (1 + 1 + 1 + 1 + 1 + 1) //YEARN + AMOUNT + VAULT + NETWORK + GAS_FEES + APPROVE
+const contractAddr = "0x950027632fbd6adadfe82644bfb64647642b6c09";
+const AMOUNT_TO_REPAY = '4358900000000';
+const BASE_SCREENS_S = (1 + 1 + 1 + 1 + 1) //YEARN + AMOUNT + GAS_FEES + VAULT + APPROVE
+const BASE_SCREENS_X = (1 + 1 + 1 + 1 + 1) //YEARN + AMOUNT + GAS_FEES + VAULT + APPROVE
 
+// Nanos S test
 test('[Nano S] IB Repay Borrow', zemu("nanos", async (sim, eth) => {
-  // Original TX: https://etherscan.io/tx/0x7bbd85e2d3f685f43905b5ad556f5339f1afac36686187e4619f637c60c4c7c3
-  eth.signTransaction(
-    "44'/60'/0'/0/0",
-    "f88b821d99850d8111c4008306cb5394950027632fbd6adadfe82644bfb64647642b6c0980a40e752702000000000000000000000000000000000000000000000000000003f6e2afa50026a0f86a8734ecaf9785c8e4332c1cb1dc0f6a3edc260e461974eaa81248c7b66faaa05ebb38fcdf2ba63b1b862b0c912a2e4123b9bdb55429bb0f0eb3412b4c69c67d"
-  );
+  const contract = new ethers.Contract(contractAddr, ['function repayBorrow(uint256 repayAmount)']);
+  const {data} = await contract.populateTransaction.repayBorrow(AMOUNT_TO_REPAY);
+  let unsignedTx = genericTx;
+  unsignedTx.to = contractAddr;
+  unsignedTx.data = data;
+
+  const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
+  const tx = eth.signTransaction("44'/60'/0'/0", serializedTx);
 
   await waitForAppScreen(sim);
-  await sim.navigateAndCompareSnapshots('.', 'nanos_ib_repay_borrow', [BASE_SCREENS_S, 0]);
+  await sim.navigateAndCompareSnapshots('.', 'nanos_ib_borrow', [BASE_SCREENS_S, 0]);
+  await tx;
 }));
 
+test('[Nano X] IB Repay Borrow', zemu("nanox", async (sim, eth) => {
+  const contract = new ethers.Contract(contractAddr, ['function repayBorrow(uint256 repayAmount)']);
+  const {data} = await contract.populateTransaction.repayBorrow(AMOUNT_TO_REPAY);
+  let unsignedTx = genericTx;
+  unsignedTx.to = contractAddr;
+  unsignedTx.data = data;
 
-
-test('[Nano S] IB Repay Borrow', zemu("nanox", async (sim, eth) => {
-  // Original TX: https://etherscan.io/tx/0x7bbd85e2d3f685f43905b5ad556f5339f1afac36686187e4619f637c60c4c7c3
-  eth.signTransaction(
-    "44'/60'/0'/0/0",
-    "f88b821d99850d8111c4008306cb5394950027632fbd6adadfe82644bfb64647642b6c0980a40e752702000000000000000000000000000000000000000000000000000003f6e2afa50026a0f86a8734ecaf9785c8e4332c1cb1dc0f6a3edc260e461974eaa81248c7b66faaa05ebb38fcdf2ba63b1b862b0c912a2e4123b9bdb55429bb0f0eb3412b4c69c67d"
-  );
+  const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
+  const tx = eth.signTransaction("44'/60'/0'/0", serializedTx);
 
   await waitForAppScreen(sim);
-  await sim.navigateAndCompareSnapshots('.', 'nanox_ib_repay_borrow', [BASE_SCREENS_X, 0]);
+  await sim.navigateAndCompareSnapshots('.', 'nanox_ib_borrow', [BASE_SCREENS_X, 0]);
+  await tx;
 }));
