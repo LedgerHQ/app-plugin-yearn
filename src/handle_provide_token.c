8fc79766107c747fe6961ee6b35a69ebb69ae656
Major-Eth <major-tom.eth@pm.me>
2022-02-14 10:39:38 +0100

    ♻️ Updating handle_provide_token

--- src/handle_provide_token.c
+++ src/handle_provide_token.c
@@ -5,15 +5,15 @@ const uint8_t NULL_ETH_ADDRESS[ADDRESS_LENGTH] = {0x00, 0x00, 0x00, 0x00, 0x00, 
                                                   0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 void handle_provide_token(void *parameters) {
-    ethPluginProvideToken_t *msg = (ethPluginProvideToken_t *) parameters;
+    ethPluginProvideInfo_t *msg = (ethPluginProvideInfo_t *) parameters;
     context_t *context = (context_t *) msg->pluginContext;
 
     if (memcmp(context->extra_address, NULL_ETH_ADDRESS, ADDRESS_LENGTH) == 0) {
         context->decimals = 18;
         strlcpy(context->want, "ETH", sizeof(context->want));
-    } else if (msg->token1) {
-        context->decimals = msg->token1->decimals;
-        strlcpy(context->want, (char *) msg->token1->ticker, sizeof(context->want));
+    } else if (msg->item1) {
+        context->decimals = msg->item1->decimals;
+        strlcpy(context->want, (char *) msg->item1->ticker, sizeof(context->want));
     } else {
         context->decimals = 18;
         strlcpy(context->want, "???", sizeof(context->want));
