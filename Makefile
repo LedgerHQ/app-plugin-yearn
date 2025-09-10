e602c70ea2ff1556aea8f98b3c310a6ceb62ae75
Francois Beutin <francois.beutin@ledger.fr>
2025-09-10 11:46:30 +0200

    Add support for APEX_P

--- Makefile
+++ Makefile
@@ -19,7 +19,7 @@ APPNAME = "Yearn"
 
 # Application version
 APPVERSION_M = 1
-APPVERSION_N = 3
+APPVERSION_N = 4
 APPVERSION_P = 0
 
 include ethereum-plugin-sdk/standard_plugin.mk
--- src/yearn_plugin.h
+++ src/yearn_plugin.h
@@ -62,6 +62,6 @@ typedef struct context_t {
     selector_t selectorIndex;
 } context_t;
 
-// Piece of code that will check that the above structure is not bigger than 5 * 32. Do not remove
-// this check.
-_Static_assert(sizeof(context_t) <= 5 * 32, "Structure of parameters too big.");
+// Check that the plugin context structure will fit in the ethereum allocated memory.
+// Do not remove this check.
+ASSERT_SIZEOF_PLUGIN_CONTEXT(context_t);
